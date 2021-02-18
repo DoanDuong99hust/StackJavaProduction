f6da3702338dbd9ef198cee7d8189d042eefebbc
DoanDuong <doanduong1999@gmail.com>
2021-02-18 10:37:24 +0700

    upload file to a individual folder

---
+++ UploadDownLoadFile/src/main/java/com/doanduong/files/controller/App.java
@@ -0,0 +1,9 @@
+package com.doanduong.files.controller;
+
+public class App {
+    public static void main(String[] args) {
+        BaseController controller = new BaseController();
+        System.out.println(controller.getFolderUpload());
+
+    }
+}
--- UploadDownLoadFile/src/main/java/com/doanduong/files/controller/BaseController.java
+++ UploadDownLoadFile/src/main/java/com/doanduong/files/controller/BaseController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.Part;
 import java.io.File;
+import java.util.Date;
 
 @Controller
 public class BaseController {
@@ -21,16 +23,30 @@ public class BaseController {
         model.addAttribute("message","upload success");
         model.addAttribute("description", myfile.getDescription());
 
-
+        // Chuyển file upload đến thư mục
         try {
             MultipartFile multipartFile = myfile.getMultipartFile();
             String fileName = multipartFile.getOriginalFilename();
-            File file = new File("D:/", fileName);
+            File file = new File(getFolderUpload(), fileName);
             multipartFile.transferTo(file);
         } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("message", "upload failed!");
         }
         return "result";
+    }
+
+    // tạo đường dẫn và thư mục cho từng file upload
+    public static final String SAVE_DIRECTORY = "testUploadFiles";
+
+    public File getFolderUpload() {
+        String appPath = "D:\\MultipleTest\\";
+
+        appPath += SAVE_DIRECTORY + File.separator + new Date().getTime();
+        File file = new File(appPath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return file;
     }
 }
